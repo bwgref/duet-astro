cbd260f2e7c96eebd912ebb2bcb657b1b3d56025
Brian Grefenstette <bwgref@srl.caltech.edu>
2019-02-12 10:42:49 -0800

    Updates from Mike
    On the telescope transmission...assuming a 6 cm by 8 cm focal plane array (i.e. three stripes). Unclear if this includes the reflectivity...

--- tdsat_telescope.py
+++ tdsat_telescope.py
@@ -24,28 +24,28 @@ def load_telescope_parameters(version, **kwargs):
         qe = 0.8 # To be improved later.
         diameter = 30*ur.cm
         psf_fwhm = 10*ur.arcsec
-        efficiency = 0.87
+        efficiency = 0.87 # Ultrasat spec
     if version == 1:
         qe = 0.8
-        efficiency = 0.45
+        efficiency = 0.54 # Reported from Mike  
         diameter = 21 * ur.cm
         psf_fwhm = 4 * ur.arcsec
     if version == 2:
         qe = 0.8
-        efficiency = 0.67 # Reported from Mike
+        efficiency = 0.65 # Reported from Mike
         diameter = 30 * ur.cm
         psf_fwhm = 9*ur.arcsec
     
     if version == 3:
         qe = 0.8
         diameter = 35*ur.cm
-        efficiency = 0.70 # Assumed
+        efficiency = 0.67 # Reported from Mike
         psf_fwhm = 18*ur.arcsec
 
     if version == 4:
         qe = 0.8
         diameter = 40*ur.cm
-        efficiency = 0.75 # Assumed
+        efficiency = 0.70 # Reported from Mike
         psf_fwhm = 23*ur.arcsec
 
     if diag:
