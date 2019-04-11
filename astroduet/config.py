59a2dfaee22f36d17146665638ecb9e7148d9bac
Brian Grefenstette <bwgref@srl.caltech.edu>
2019-04-11 16:08:39 -0700

    config docstring and testing

--- astroduet/config.py
+++ astroduet/config.py
@@ -1,7 +1,63 @@
 from astropy import units as u
-from numpy import pi, sqrt
+from numpy import pi, sqrt, allclose
 
 class Telescope():
+    """
+    Make a Telescope object containing various instrument parameters
+    
+    Parameters
+    ----------
+    on_axis : conditional, default True
+        Use the on-axis effective area and PSF sizes or use the 
+        edge of the field of view values.
+
+
+    Returns
+    ----------
+    An initialized instance of the Telescope class.
+
+
+
+    Attributes
+    ----------
+    epd: float
+        The physical size of the entrance pupil.
+
+    eff_epd: float
+        The effective size of the entrance pupil (i.e. once corrected
+        for vignetting, with Astropy units.
+
+    psf_fwhm: float
+        The FWHM of the PSF, with Astropy units.
+                
+    psf_jitter: float
+        The contribution of the PSF to be added in quadrature with the psf_fwhm
+        due to spacecraft pointing jitter.
+        
+    psf_size: float
+        psf_fwhm and psf_jitter added in quadrature with Astropy units.
+        
+    pixel: float
+        Angular pixel size with Astropy units
+
+    bandone: 1-d float array
+        ``[eff_band, eff_width]``
+
+    bandtwo: 1-d float array
+        ``[eff_band, eff_width]``
+    
+    eff_area: float
+        Effective area computed using the eff_epd size.
+    
+    Examples
+    --------
+    >>> duet = Telescope()
+    >>> allclose(duet.eff_epd.value, 24.2)
+    True
+    
+
+    """
+
     def __init__(self, on_axis=True):
         self.EPD = 30*u.cm
         
@@ -37,7 +93,7 @@ class Telescope():
         self.bandtwo=[center_D2 - 0.5*width_D2, center_D2+0.5*width_D2]*u.nm
         
         
-    def diag(self):
+    def info(self):
         print('Physical Entrance Pupil: {}'.format(self.EPD))
         print('Effective EPD: {:5.3}'.format(self.eff_epd))
         print('Effective Area: {:8.3}'.format(self.eff_area))        
