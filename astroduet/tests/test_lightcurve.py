d794e3b54c9baf5bbe4b964b9f0e0473ca0f817f
Matteo Bachetti <matteo@matteobachetti.it>
2019-04-22 12:09:15 -0700

    Add debug information to lightcurve.py

--- astroduet/tests/test_lightcurve.py
+++ astroduet/tests/test_lightcurve.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import shutil
 from astroduet.lightcurve import get_lightcurve, lightcurve_through_image
 from astropy.table import Table
 import astropy.units as u
@@ -15,10 +17,26 @@ def test_load_lightcurve():
 
 def test_realistic_lightcurve():
     # Dummy test
+    debug_img_dir = glob.glob('debug_imgs_*')
+    if len(debug_img_dir) > 0:
+        for d in debug_img_dir:
+            shutil.rmtree(d)
+
     lc = Table({'time': [150, 450] * u.s,
                 'photflux_D1':  [4, 6] * (1 / u.cm**2 / u.s),
                 'photflux_D2': [3, 1] * (1 / u.cm**2 / u.s)})
     lc_out = lightcurve_through_image(lc, 300 * u.s,
                                       final_resolution=600 * u.s)
+    debug_img_dir = glob.glob('debug_imgs_*')
+    assert len(debug_img_dir) == 0
+    lc_out = lightcurve_through_image(lc, 300 * u.s,
+                                      final_resolution=600 * u.s,
+                                      debug=True)
+    debug_img_dir = glob.glob('debug_imgs_*')
+    assert len(debug_img_dir) > 0
+    assert os.path.isdir(debug_img_dir[0])
+    assert len(glob.glob(os.path.join(debug_img_dir[0], '*.p'))) > 0
     assert 'photflux_D1_fiterr' in lc_out.colnames
     assert len(lc_out['time']) == 1
+
+    shutil.rmtree(debug_img_dir[0])
