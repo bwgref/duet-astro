97ba4e9e9c1c2e4d7bd3bb1f538d6c3925e1895f
Brian Grefenstette <bwgref@srl.caltech.edu>
2019-05-09 15:44:20 -0700

    Fix datafile path.

--- astroduet/utils.py
+++ astroduet/utils.py
@@ -130,7 +130,7 @@ def load_neff():
     from astropy.table import Table
     ref_file = os.path.join(datadir, 'neff_data_full.dat')
 
-    neff_table = Table.read('../astroduet/data/neff_data_full.dat', format='ascii')
+    neff_table = Table.read(ref_file, format='ascii')
        
 #    oversig, oversample, neff_center, neff_corner, neff_avg = genfromtxt(ref_file, unpack=True, skip_header=True)
     return neff_table['pix-fwhm'].data, neff_table['avg'].data
