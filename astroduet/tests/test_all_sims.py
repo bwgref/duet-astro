0ee658d7366aed2aee99cb35e318ecad5807a4ca
Matteo Bachetti <matteo@matteobachetti.it>
2019-05-23 14:34:23 -0700

    Only simulate one dataset per kind

--- astroduet/tests/test_all_sims.py
+++ astroduet/tests/test_all_sims.py
@@ -5,4 +5,5 @@ import astropy.units as u
 def test_sims():
     # Just test that it runs ok
     sims = Simulations()
-    sims.parse_emgw(diag=True)
+    sims.parse_emgw(diag=True, list_of_simulations=['kilonova_0.01.dat'])
+    sims.parse_sne(diag=True, list_of_simulations=['IIP_big'])
